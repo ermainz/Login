0d32078bde03f34e2010d50a0ed311d6f04bd398
Erik Mainz <erik.mainz@gmail.com>
2016-10-16 00:17:57 -0700

    app-nav and log in modal
    Moved log in to modal. Add universal page navbar with log in button.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AppNavComponent } from './app-nav/app-nav.component';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { MainComponent } from './main/main.component';
 import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
+    AppNavComponent,
     AppComponent,
-    SignInComponent
+    SignInComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -1,17 +1,12 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { SignInComponent } from './sign-in/sign-in.component';
+import { MainComponent } from './main/main.component';
 
 const appRoutes: Routes = [
   {
-    path: 'sign-in',
-    component: SignInComponent
-  },
-  {
     path: '',
-    redirectTo: '/sign-in',
-    pathMatch: 'full'
+    component: MainComponent
   }
 ];
 
