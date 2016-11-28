53e5271696130bf9b5c8cb19b29c9c44d46d0ae1
Erik Mainz <erik.mainz@gmail.com>
2016-11-27 23:09:01 -0800

    Added chai assertion library

--- test/api-authenticate-test.js
+++ test/api-authenticate-test.js
@@ -1,4 +1,6 @@
 var request = require('supertest');
+var expect = require('chai').expect;
+
 var app = require('../app');
 
 describe('Requests to /api/authenticate', function() {
@@ -8,6 +10,13 @@ describe('Requests to /api/authenticate', function() {
     .post('/api/authenticate')
     .send({ email: 'abcd', password: '1234'})
     .set('Content-Type', 'application/json')
-    .expect(200, done);
+    .expect(200)
+    .expect(function(resp) {
+      var body = resp.body;
+      expect(body).to.have.property('success');
+      // TODO these lines should pass
+      // expect(body).to.have.property('token');
+      // expect(body.success).to.equal(true);
+    }).end(done);
   });
 });
