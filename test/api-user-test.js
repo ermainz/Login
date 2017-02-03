bd9b9d685ebb828a0499465a3927b555a50282bb
Erik Mainz <erik.mainz@gmail.com>
2017-02-02 20:39:28 -0800

    Clear users collection before tests to not have dependencies between tests

--- test/api-user-test.js
+++ test/api-user-test.js
@@ -7,6 +7,11 @@ var testUtils = require('../test/utils/test-utilities')(app);
 
 describe('Requests to /api/user', function() {
 
+  beforeEach(function(done) {
+    var promises = [User.remove().exec()];
+    Promise.all(promises).then(() => done());
+  });
+
   it('returns 401 forbidden if unauthenticated', function(done) {
     request(app)
     .get('/api/user')
@@ -14,11 +19,14 @@ describe('Requests to /api/user', function() {
   });
 
   it('returns list of users when authenticated', function(done) {
-    var email = 'testusertoauth@example.com';
-    var password = 'abcd1234';
-    var user = new User({email, password});
-    user.save()
-    .then(() => testUtils.withAuth(email, password))
+    var email1 = 'testusertoauth1@example.com';
+    var email2 = 'testusertoauth2@example.com';
+    var password1 = 'abcd1234';
+    var password2 = 'abcd2468';
+    var user1 = new User({email: email1, password: password1});
+    var user2 = new User({email: email2, password: password2});
+    Promise.all([user1.save(), user2.save()])
+    .then(() => testUtils.withAuth(email1, password1))
     .then(jwt => {
       request(app)
       .get('/api/user')
@@ -26,7 +34,7 @@ describe('Requests to /api/user', function() {
       .expect(200)
       .expect('Content-Type', /json/)
       .expect(function(resp) {
-        expect(resp.body.length).to.equal(3);
+        expect(resp.body.length).to.equal(2);
       }).end(done);
     });
   });
